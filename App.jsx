0430063683e4bd2ffc07ed3300b16112bb42bef4
MatheusHenrique564 <matheus.henrique.senai564@gmail.com>
2024-02-21 09:27:14 -0300

    first commit

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+
+export default function App() {
+	return (
+		<View>
+      <Text>Hello, World!</Text>
+      <StatusBar style="auto" />
+    </View>
+	);
+}
